46bcaf84cbc806fc34213c3fb88a62bec7d761e4
Karel Kočí <cynerd@email.cz>
2016-03-31 20:11:43 +0200

    Change setup script and add ignores for build folder
    Fixing setup script while setting up project to pypi.

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
-from setuptools import setup, find_packages
 from os import path
+from setuptools import setup
 
-here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+readme_file = path.join(path.abspath(path.dirname(__file__)), 'README.md')
+long_description = ""
+try:
+    import pypandoc
+    long_description = pypandoc.convert(readme_file, 'rst', format='md')
+except (IOError, ImportError):
+    print("Pandoc not found. Long_description conversion failure.")
 
 
 setup(
     name='mcserver-wrapper',
-    version='0.3.0',
+    version='0.3.6',
     description="Minecraft server wrapper",
     long_description=long_description,
     url="https://github.com/Cynerd/mcserver-wrapper",
@@ -17,7 +21,7 @@ setup(
     author_email="cynerd@email.cz",
     license="GPLv2",
 
-    clasifiers=[
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
